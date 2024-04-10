b300613a6f8d8da87d1cfc9dd20652a8c2269628
Gabrielogui <gabrielcsro@gmail.com>
2024-04-10 19:50:44 -0300

    Organizando o código

--- trabalho.py
+++ trabalho.py
@@ -2,12 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+# ======= LENDO O ARQUIVO =======
 dados = pd.read_csv("salaries.csv")
 
 df = pd.DataFrame(dados)
 
+# ======= MÉDIA DOS SALÁRIOS PARA TRABALHO =======
 job_title_media = dados.groupby("job_title")["salary_in_usd"].mean()
+
+# ======= PLOT DO GRÁFICO 'TRABALHO X SALARIO(USD)' =======
 
 barra_salarie_per_jobtitle = plt.figure("grafico_de_barras_salarie_per_jobtitle")
 
@@ -15,22 +18,17 @@ job_title_media.plot.bar()
 
 plt.show()
 
+# ======= MÉDIA DOS SALÁRIOS PARA NÍVEL DE EXPERIÊNCIA =======
 experience_level_media = dados.groupby("experience_level")["salary_in_usd"].mean()
 
+# ======= PLOT DO GRÁFICO 'EXPERIÊNCIA X SALÁRIAO(USD)' =======
 barra_salarie_per_experience_level = plt.figure("grafico_de_barras_salarie_per_experience_level")
 
 experience_level_media.plot.bar()
 
 plt.show()
 
-
-#jobtitle.hist(bins=2)
-
-#df_media = pd.DataFrame(dados['salary_in_usd'], dados['job_title'])
-
-
-#df_media.plot.bar()
-
+# ======= ESTATÍSTICAS =======
 moda = df['salary_in_usd'].mode().values[0]
 print("A moda da coluna é:", moda)
 
@@ -48,18 +46,14 @@ print("A curtose da coluna é:", curtose)
 
 desvio_padrao = df['salary_in_usd'].std()
 print("O desvio padrão da coluna é:", desvio_padrao)
+# ============================
 
-
-#não sei oq isso significa '-'
-
-# Carregar dados do arquivo CSV usando pandas
-dados = pd.read_csv('salaries.csv')
-
-# Extrair ângulos e valores do DataFrame
+# ======= GRÁFICO POLAR =======
+# Extrair ângulos e valores do DataFrame:
 angulos = dados['salary_in_usd'].values
 valores = dados['experience_level'].values
 
-# Criar um gráfico polar
+# Criar um gráfico polar:
 plt.figure(figsize=(8, 8))
 ax = plt.subplot(111, polar=True)
 
@@ -74,10 +68,9 @@ ax.set_theta_zero_location('N')
 ax.set_theta_direction(-1)
 plt.title('Gráfico Polar')
 plt.show()
+# =============================
 
-# Carregar os dados do arquivo CSV
-dados = pd.read_csv('salaries.csv')
-
+# ======= CARTOGRAMA =======
 # Preparar os dados
 # Suponha que seu arquivo CSV tenha colunas 'Região' e 'Variável'
 # Substitua 'Região' pelo nome da coluna que contém as informações geográficas e
@@ -98,12 +91,9 @@ plt.xticks(rotation=45)  # Rotacionar os rótulos no eixo x para facilitar a lei
 plt.tight_layout()
 plt.show()
 
+# ==========================
 
-
-
-# Carregar os dados do arquivo CSV
-dados = pd.read_csv('salaries.csv')
-
+# ======= PICTOGRAMA =======
 # Extrair categorias e contagens do DataFrame
 categorias = dados['experience_level']
 contagens = dados['salary_in_usd']
@@ -122,6 +112,7 @@ plt.title('Pictograma')
 plt.legend()
 plt.tight_layout()
 plt.show()
+# ==========================
 
 # ESTATÍSTICAS:
 '''
@@ -133,87 +124,3 @@ quartis
 variancia
 '''
 
-# while True:
-#     print("\n=== MENU ===")
-#     print("1. Adicionar profissional")
-#     print("2. Listar profissionais em ordem alfabetica")
-#     print("3. Buscar profissional")
-#     print("4. Remover profissional")
-#     print("5. Remover relacionamento")
-#     print("6. Salvar e sair")
-#     opcao = input("Escolha uma opção: ")
-
-#     if opcao == '1':
-#         nome = input('Digite o nome do profissional: ')
-#         if nome != '0':
-#             profissionais[nome] = {}
-#             profissionais[nome]['cargo'] = input('Digite o cargo: ')
-#             profissionais[nome]['salario'] = input('Digite o salario: ')
-#             profissionais[nome]['lista'] = []
-#             while True:
-#                 print('Digite o relacionamento, ou digite 0 para parar')
-#                 resposta = input()
-#                 if resposta == '0':
-#                     break
-#                 else:
-#                     profissionais[nome]['lista'].append(resposta)
-#         else:
-#             break
-
-
-#     elif opcao == '2':
-#         # Imprime o dicionário ordenado
-#         dicionario_ordenado = dict(sorted(profissionais.items()))
-#         for chave, valor in dicionario_ordenado.items():
-#             print(f'{chave}: {valor}')
-
-
-#     elif opcao == '3':
-#         nome = input('Digite o nome do profissional: ')
-#         if nome in profissionais:
-#             print(f'Nome: {nome}')
-#             print(f'Cargo: {profissionais[nome]["cargo"]}')
-#             print(f'Salário: {profissionais[nome]["salario"]}')
-#             print(f'Relacionamentos:')
-#             for relacionamento in profissionais[nome]['lista']:
-#                 print(f'- {relacionamento}')
-#         else:
-#             print(f'O nome "{nome}" não foi encontrado no dicionário.')
-
-
-#     elif opcao == '4':
-#         remove = input("Digite o nome do profissional que deseja remover: ")
-#         if remove in profissionais:
-#             profissionais.pop(remove)
-#             print(f"{remove} foi removido com sucesso!")
-#         else:
-#             print(f"{remove} não encontrado na lista de profissionais.")
-
-
-#     elif opcao == '5':
-#         profissional = input("Digite o nome do profissional que deseja alterar: ")
-#         remove_relacionamento = input(f"Digite o nome do relacionamento de {profissional} que deseja remover: ")
-#         if profissional in profissionais and remove_relacionamento in profissionais[profissional]['lista']:
-#             profissionais[profissional]['lista'].remove(remove_relacionamento)
-#             print(f"Relacionamento '{remove_relacionamento}' removido com sucesso para o profissional '{profissional}'.")
-#         else:
-#             print(f"O relacionamento '{remove_relacionamento}' não encontrado na lista de relacionamentos de '{profissional}'.")
-    
-    
-#     elif opcao == '6':
-#         #salvar em json
-#         nome_arquivo = 'profissionais.json'
-#         with open(nome_arquivo, 'w') as arquivo:
-#             json.dump(profissionais, arquivo)
-
-#         #salvar em .txt
-#         nome_arquivo = 'profissionais.txt'
-#         with open(nome_arquivo, 'w') as arquivo:
-#             for chave, valor in profissionais.items():
-#                 arquivo.write(f'{chave}: {valor}\n')
-#         print("Dados salvos. Saindo do programa.")
-#         break
-    
-    
-#     else:
-#         print("Opção inválida. Por favor, escolha uma opção válida.")
