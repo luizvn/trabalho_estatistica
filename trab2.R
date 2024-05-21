ca0e19326d24a122ec74795ac4b4ca1bd3b7b5ca
Gabrielogui <gabrielcsro@gmail.com>
2024-05-21 20:23:50 -0300

    Teste Z finalizado

--- trab2.R
+++ trab2.R
@@ -8,32 +8,39 @@ library(BSDA)
 data <- read.csv("salaries.csv")
 
 # Verificar a estrutura dos dados
-str(data)
+# str(data)
 
-# Converter a coluna 'salaries_in_usd' para numérica, se necessário
-data$salaries_in_usd <- as.numeric(as.character(data$salaries_in_usd))
+# Verificar os primeiros valores da coluna 'salaries_in_usd'
+head(data$salary_in_usd)
 
-# Filtrar os dados para incluir apenas as linhas onde 'company_location' é 'US' e 'experience_level' é 'SE'
+# Verificar se há valores não numéricos na coluna 'salaries_in_usd'
+non_numeric_values <- data$salary_in_usd[!grepl("^\\d+$", data$salary_in_usd)]
+# print(non_numeric_values)
+
+# Converter a coluna 'salaries_in_usd' para numérica
+data$salary_in_usd <- as.numeric(as.character(data$salary_in_usd))
+
+# Filtrar os dados para incluir apenas as e 'experience_level' é 'SE'
 data_us_se <- data %>%
-  filter(company_location == "US", experience_level == "SE")
+  filter(experience_level == "SE")
 
 # Calcular a média populacional conhecida (média geral de 'salaries_in_usd')
-pop_mean <- mean(data$salaries_in_usd, na.rm = TRUE)
+pop_mean <- mean(data$salary_in_usd, na.rm = TRUE)
 
 # Calcular o desvio padrão populacional conhecido (desvio padrão geral de 'salaries_in_usd')
-pop_sd <- sd(data$salaries_in_usd, na.rm = TRUE)
+pop_sd <- sd(data$salary_in_usd, na.rm = TRUE)
+print(paste("Desvio padrão: ", pop_sd))
 
 # Calcular a média e outras estatísticas descritivas necessárias para a amostra filtrada
-sample_mean <- mean(data_us_se$salaries_in_usd, na.rm = TRUE)
-sample_size <- sum(!is.na(data_us_se$salaries_in_usd))
+sample_mean <- mean(data_us_se$salary_in_usd, na.rm = TRUE)
+sample_size <- sum(!is.na(data_us_se$salary_in_usd))
 
 # Verificar a média populacional e a amostral
 print(paste("Média populacional:", pop_mean))
 print(paste("Média da amostra:", sample_mean))
 
 # Realizar o teste Z
-z_result <- z.test(x = data_us_se$salaries_in_usd, mu = pop_mean, sigma.x = pop_sd, alternative = "two.sided")
+z_result <- z.test(x = data_us_se$salary_in_usd, mu = pop_mean, sigma.x = pop_sd, alternative = "two.sided")
 
 # Exibir o resultado do teste Z
 print(z_result)
-
